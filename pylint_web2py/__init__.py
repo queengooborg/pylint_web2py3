54ff4d77a97ef2db6fdad05fae198761490f24c0
Alexander Presnyakov <flagist0@gmail.com>
2014-11-13 02:01:32 +0400

    Switched to new Astroid API Added models and modules as web2py targets

--- pylint_web2py/__init__.py
+++ pylint_web2py/__init__.py
@@ -1,10 +1,15 @@
-from logilab.astng import MANAGER
-from logilab.astng.builder import ASTNGBuilder
+from astroid import MANAGER
+from astroid import scoped_nodes
+from astroid.builder import AstroidBuilder
+import re
+
+web2py_component_regex = re.compile(r'.+?(models|views|controllers|modules)')
 
 def web2py_transform(module):
-    if 'controllers' in module.name or 'views' in module.name:
+    #Currently module.name and module.file are empty because of Astroid bug
+    if module.file and re.match(web2py_component_regex, module.file):
         # This dummy code is copied from gluon/__init__.py
-        fake = ASTNGBuilder(MANAGER).string_build('''\
+        fake = AstroidBuilder(MANAGER).string_build('''\
 from gluon.globals import current
 from gluon.html import *
 from gluon.validators import *
@@ -36,7 +41,5 @@ plugins = PluginManager()
 ''')
         module.locals.update(fake.locals)
 
-
-
 def register(linter):
-    MANAGER.register_transformer(web2py_transform)
+    MANAGER.register_transform(scoped_nodes.Module, web2py_transform)
