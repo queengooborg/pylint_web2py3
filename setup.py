0db1e2039b88212f75b3d69b79de88cb1b087b58
Alexander Presnyakov <flagist0@gmail.com>
2014-12-02 07:02:52 +0400

    Switched to Astroid 1.3.0

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ setup(
     description=_short_description,
     version=_version,
     packages=_packages,
-    dependency_links=['hg+https://bitbucket.org/logilab/astroid/get/tip.zip#egg=astroid-1.2.2'],
-    install_requires=['astroid>=1.2.2', #Hack for pip to use mercurial version. When 1.2.2 is out, pip will use it
+    install_requires=['astroid>=1.3.0',
                       'pylint>=1.2.0'],
     license='GPLv2',
     keywords='pylint web2py plugin',
