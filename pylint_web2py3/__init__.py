dbc223dc31bb1889ef161d8ad4b35b4a914d20bf
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-10-18 02:35:17 -0700

    Convert spaces to tabs

--- pylint_web2py3/__init__.py
+++ pylint_web2py3/__init__.py
@@ -34,24 +34,24 @@ from pylint.interfaces import UNDEFINED
 from pylint.utils import ASTWalker
 
 def register(_):
-    '''Register web2py transformer, called by pylint'''
-    MANAGER.register_transform(scoped_nodes.Module, web2py_transform)
+	'''Register web2py transformer, called by pylint'''
+	MANAGER.register_transform(scoped_nodes.Module, web2py_transform)
 
 def web2py_transform(module):
-    '''Add imports and some default objects, add custom module paths to pythonpath'''
+	'''Add imports and some default objects, add custom module paths to pythonpath'''
 
-    if module.file:
-        #Check if this file belongs to web2py
-        web2py_match = re.match(r'(.+?)/applications/(.+?)/(.+?)/', module.file)
-        if web2py_match:
-            web2py_path, app_name, subfolder = web2py_match.group(1), web2py_match.group(2), web2py_match.group(3)
-            return transformer.transform_module(module, web2py_path, app_name, subfolder)
+	if module.file:
+		#Check if this file belongs to web2py
+		web2py_match = re.match(r'(.+?)/applications/(.+?)/(.+?)/', module.file)
+		if web2py_match:
+			web2py_path, app_name, subfolder = web2py_match.group(1), web2py_match.group(2), web2py_match.group(3)
+			return transformer.transform_module(module, web2py_path, app_name, subfolder)
 
 class Web2PyTransformer(object):
-    '''Transforms web2py modules code'''
-    # This dummy code is copied from gluon/__init__.py and gluon/compileapp.py
-    # First two lines are copied from ALL array in gluon.html, gluon.validators, and pydal.validators
-    fake_code = '''
+	'''Transforms web2py modules code'''
+	# This dummy code is copied from gluon/__init__.py and gluon/compileapp.py
+	# First two lines are copied from ALL array in gluon.html, gluon.validators, and pydal.validators
+	fake_code = '''
 from gluon.html import A, ASSIGNJS, B, BEAUTIFY, BODY, BR, BUTTON, CENTER, CAT, CODE, COL, COLGROUP, DIV, EM, EMBED, FIELDSET, FORM, H1, H2, H3, H4, H5, H6, HEAD, HR, HTML, I, IFRAME, IMG, INPUT, LABEL, LEGEND, LI, LINK, OL, UL, MARKMIN, MENU, META, OBJECT, ON, OPTION, P, PRE, SCRIPT, OPTGROUP, SELECT, SPAN, STRONG, STYLE, TABLE, TAG, TD, TEXTAREA, TH, THEAD, TBODY, TFOOT, TITLE, TR, TT, URL, XHTML, XML, xmlescape, embed64
 from gluon.validators import ANY_OF, CLEANUP, CRYPT, IS_ALPHANUMERIC, IS_DATE_IN_RANGE, IS_DATE, IS_DATETIME_IN_RANGE, IS_DATETIME, IS_DECIMAL_IN_RANGE, IS_EMAIL, IS_LIST_OF_EMAILS, IS_EMPTY_OR, IS_EXPR, IS_FILE, IS_FLOAT_IN_RANGE, IS_IMAGE, IS_IN_DB, IS_IN_SET, IS_INT_IN_RANGE, IS_IPV4, IS_IPV6, IS_IPADDRESS, IS_LENGTH, IS_LIST_OF, IS_LOWER, IS_MATCH, IS_EQUAL_TO, IS_NOT_EMPTY, IS_NOT_IN_DB, IS_NULL_OR, IS_SLUG, IS_STRONG, IS_TIME, IS_UPLOAD_FILENAME, IS_UPPER, IS_URL, IS_JSON, simple_hash, get_digest, Validator, ValidationError, translate
 
@@ -74,150 +74,150 @@ session = Session()
 cache = Cache(request)
 T = translator(request)
 local_import = lambda name, reload=False, app=request.application:\
-        local_import_aux(name, reload, app)
+		local_import_aux(name, reload, app)
 '''
 
-    def __init__(self):
-        '''
-        self.top_level: are we dealing with the original passed file?
-        Pylint will recursively parse imports and models, we don't want to transform them
-        '''
-        self.is_pythonpath_modified = False
-        self.app_model_names = []
-        self.top_level = True
+	def __init__(self):
+		'''
+		self.top_level: are we dealing with the original passed file?
+		Pylint will recursively parse imports and models, we don't want to transform them
+		'''
+		self.is_pythonpath_modified = False
+		self.app_model_names = []
+		self.top_level = True
 
-    def transform_module(self, module_node, web2py_path, app_name, subfolder):
-        '''Determine the file type (model, controller or module) and transform it'''
-        if not self.top_level:
-            return module_node
+	def transform_module(self, module_node, web2py_path, app_name, subfolder):
+		'''Determine the file type (model, controller or module) and transform it'''
+		if not self.top_level:
+			return module_node
 
-        #Add web2py modules paths to sys.path
-        self._add_paths(web2py_path, app_name)
+		#Add web2py modules paths to sys.path
+		self._add_paths(web2py_path, app_name)
 
-        if subfolder == 'models':
-            self.top_level = False
-            transformed_module = self._trasform_model(module_node)
-            self.top_level = True
-        elif subfolder == 'controllers':
-            self.top_level = False
-            transformed_module = self._transform_controller(module_node)
-            self.top_level = True
-        else:
-            transformed_module = module_node
+		if subfolder == 'models':
+			self.top_level = False
+			transformed_module = self._trasform_model(module_node)
+			self.top_level = True
+		elif subfolder == 'controllers':
+			self.top_level = False
+			transformed_module = self._transform_controller(module_node)
+			self.top_level = True
+		else:
+			transformed_module = module_node
 
-        return transformed_module
+		return transformed_module
 
-    def _add_paths(self, web2py_path, app_name):
-        '''Add web2py module paths models path to sys.path to be able to import it from the fake code'''
-        if not self.is_pythonpath_modified:
-            gluon_path = os.path.join(web2py_path, 'gluon')
-            site_packages_path = os.path.join(web2py_path, 'site-packages')
-            app_modules_path = os.path.join(web2py_path, 'applications', app_name, 'modules')
-            app_models_path = os.path.join(web2py_path, 'applications', app_name, 'models') #Add models to import them them in controllers
+	def _add_paths(self, web2py_path, app_name):
+		'''Add web2py module paths models path to sys.path to be able to import it from the fake code'''
+		if not self.is_pythonpath_modified:
+			gluon_path = os.path.join(web2py_path, 'gluon')
+			site_packages_path = os.path.join(web2py_path, 'site-packages')
+			app_modules_path = os.path.join(web2py_path, 'applications', app_name, 'modules')
+			app_models_path = os.path.join(web2py_path, 'applications', app_name, 'models') #Add models to import them them in controllers
 
-            for module_path in [gluon_path, site_packages_path, app_modules_path, app_models_path, web2py_path]:
-                sys.path.append(module_path)
+			for module_path in [gluon_path, site_packages_path, app_modules_path, app_models_path, web2py_path]:
+				sys.path.append(module_path)
 
-            self._fill_app_model_names(app_models_path)
+			self._fill_app_model_names(app_models_path)
 
-            self.is_pythonpath_modified = True
+			self.is_pythonpath_modified = True
 
 
-    def _trasform_model(self, module_node):
-        '''Add globals from fake code + import code from previous (in alphabetical order) models'''
-        fake_code = self.fake_code + self._gen_models_import_code(module_node.name)
-        fake = AstroidBuilder(MANAGER).string_build(fake_code)
-        module_node.locals.update(fake.globals)
+	def _trasform_model(self, module_node):
+		'''Add globals from fake code + import code from previous (in alphabetical order) models'''
+		fake_code = self.fake_code + self._gen_models_import_code(module_node.name)
+		fake = AstroidBuilder(MANAGER).string_build(fake_code)
+		module_node.locals.update(fake.globals)
 
-        module_node = self._remove_unused_imports(module_node, fake)
-        return module_node
+		module_node = self._remove_unused_imports(module_node, fake)
+		return module_node
 
-    def _transform_controller(self, module_node):
-        '''Add globals from fake code + import models'''
-        fake_code = self.fake_code + self._gen_models_import_code()
+	def _transform_controller(self, module_node):
+		'''Add globals from fake code + import models'''
+		fake_code = self.fake_code + self._gen_models_import_code()
 
-        fake = AstroidBuilder(MANAGER).string_build(fake_code)
-        module_node.locals.update(fake.globals)
+		fake = AstroidBuilder(MANAGER).string_build(fake_code)
+		module_node.locals.update(fake.globals)
 
-        module_node = self._remove_unused_imports(module_node, fake)
+		module_node = self._remove_unused_imports(module_node, fake)
 
-        return module_node
+		return module_node
 
-    def _gen_models_import_code(self, current_model=None):
-        '''Generate import code for models (only previous in alphabetical order if called by model)'''
-        code = ''
-        for model_name in self.app_model_names:
-            if current_model and model_name == current_model:
-                break
-            code += 'from %s import *\n' % model_name
+	def _gen_models_import_code(self, current_model=None):
+		'''Generate import code for models (only previous in alphabetical order if called by model)'''
+		code = ''
+		for model_name in self.app_model_names:
+			if current_model and model_name == current_model:
+				break
+			code += 'from %s import *\n' % model_name
 
-        return code
+		return code
 
-    def _fill_app_model_names(self, app_models_path):
-        '''Save model names for later use'''
-        model_files = os.listdir(app_models_path)
-        model_files = [model_file for model_file in model_files if re.match(r'.+?\.py$', model_file)] #Only top-level models
-        model_files = sorted(model_files) #Models are executed in alphabetical order
-        self.app_model_names = [re.match(r'^(.+?)\.py$', model_file).group(1) for model_file in model_files]
+	def _fill_app_model_names(self, app_models_path):
+		'''Save model names for later use'''
+		model_files = os.listdir(app_models_path)
+		model_files = [model_file for model_file in model_files if re.match(r'.+?\.py$', model_file)] #Only top-level models
+		model_files = sorted(model_files) #Models are executed in alphabetical order
+		self.app_model_names = [re.match(r'^(.+?)\.py$', model_file).group(1) for model_file in model_files]
 
-    def _remove_unused_imports(self, module_node, fake_node):
-        '''We import objects from fake code and from models, so pylint doesn't complain about undefined objects.
+	def _remove_unused_imports(self, module_node, fake_node):
+		'''We import objects from fake code and from models, so pylint doesn't complain about undefined objects.
 But now it complains a lot about unused imports.
 We cannot suppress it, so we call VariableChecker with fake linter to intercept and collect all such error messages,
 and then use them to remove unused imports.'''
-        #Needed for removal of unused import messages
-        sniffer = MessageSniffer() #Our linter substitution
-        walker = ASTWalker(sniffer)
-        var_checker = VariablesChecker(sniffer)
-        comp_checker = ComparisonChecker(sniffer)
-        walker.add_checker(var_checker)
-        walker.add_checker(comp_checker)
+		#Needed for removal of unused import messages
+		sniffer = MessageSniffer() #Our linter substitution
+		walker = ASTWalker(sniffer)
+		var_checker = VariablesChecker(sniffer)
+		comp_checker = ComparisonChecker(sniffer)
+		walker.add_checker(var_checker)
+		walker.add_checker(comp_checker)
 
-        #Collect unused import messages
-        sniffer.set_fake_node(fake_node)
-        sniffer.check_astroid_module(module_node, walker, [], [])
+		#Collect unused import messages
+		sniffer.set_fake_node(fake_node)
+		sniffer.check_astroid_module(module_node, walker, [], [])
 
-        #Remove unneeded globals imported from fake code
-        # for name in sniffer.unused:
-        #     if name in fake_node.globals and \
-        #       name in module_node.locals: #Maybe it's already deleted
-        #         del module_node.locals[name]
+		# Remove unneeded globals imported from fake code
+		# for name in sniffer.unused:
+		# 	if name in fake_node.globals and \
+		# 	  name in module_node.locals: #Maybe it's already deleted
+		# 		del module_node.locals[name]
 
-        return module_node
+		return module_node
 
 class MessageSniffer(PyLinter):
-    '''Special class to mimic PyLinter to intercept messages from checkers. Here we use it to collect info about unused imports and singleton comparsons in db queries'''
-    def __init__(self):
-        super(MessageSniffer, self).__init__()
-        self.unused = set()
-        self.walker = None
-        self.fake_node = None
+	'''Special class to mimic PyLinter to intercept messages from checkers. Here we use it to collect info about unused imports and singleton comparsons in db queries'''
+	def __init__(self):
+		super(MessageSniffer, self).__init__()
+		self.unused = set()
+		self.walker = None
+		self.fake_node = None
 
-    def set_fake_node(self, fake_node):
-        '''We need fake node to distinguish real unused imports in user code from unused imports induced by our fake code'''
-        self.fake_node = fake_node
-        self.unused = set()
+	def set_fake_node(self, fake_node):
+		'''We need fake node to distinguish real unused imports in user code from unused imports induced by our fake code'''
+		self.fake_node = fake_node
+		self.unused = set()
 
-    def add_message(self, msg_descr, line=None, node=None, args=None, confidence=UNDEFINED, col_offset=None):
-        '''Message interceptor'''
-        if msg_descr == 'unused-wildcard-import':
-            self.unused.add(args)
+	def add_message(self, msg_descr, line=None, node=None, args=None, confidence=UNDEFINED, col_offset=None):
+		'''Message interceptor'''
+		if msg_descr == 'unused-wildcard-import':
+			self.unused.add(args)
 
-        elif msg_descr == 'unused-import':
-            #Unused module or unused symbol from module, extract with regex
-            sym_match = re.match(r'^(.+?)\ imported\ from', args)
-            if sym_match:
-                sym_name = sym_match.group(1)
-            else:
-                module_match = re.match(r'^import\ (.+?)$', args)
-                assert module_match
-                sym_name = module_match.group(1)
+		elif msg_descr == 'unused-import':
+			#Unused module or unused symbol from module, extract with regex
+			sym_match = re.match(r'^(.+?)\ imported\ from', args)
+			if sym_match:
+				sym_name = sym_match.group(1)
+			else:
+				module_match = re.match(r'^import\ (.+?)$', args)
+				assert module_match
+				sym_name = module_match.group(1)
 
-            if sym_name in self.fake_node.globals:
-                self.unused.add(sym_name)
+			if sym_name in self.fake_node.globals:
+				self.unused.add(sym_name)
 
-        elif msg_descr == 'singleton-comparison':
-            if node.as_string().startswith("db"):
-                pass # XXX Ignore singleton here
+		elif msg_descr == 'singleton-comparison':
+			if node.as_string().startswith("db"):
+				pass # XXX Ignore singleton here
 
 transformer = Web2PyTransformer()
--- setup.py
+++ setup.py
@@ -5,25 +5,25 @@ from setuptools import setup, find_packages
 _packages = find_packages()
 
 with open('README.md') as f:
-    readme = f.read()
+	readme = f.read()
 with open('requirements.txt') as f:
-    requires = [line.strip() for line in f if line.strip()]
+	requires = [line.strip() for line in f if line.strip()]
 
 setup(
-    name=pylint_web2py3.__name__,
-    url='https://github.com/vinyldarkscratch/pylint_web2py3',
-    version=pylint_web2py3.__version__,
-    description=pylint_web2py3.__doc__,
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author=pylint_web2py3.__author__[0],
-    author_email=pylint_web2py3.__email__[0],
-    maintainer=pylint_web2py3.__maintainer__,
-    maintainer_email=pylint_web2py3.__email__[0],
-    license='http://www.gnu.org/copyleft/gpl.html',
-    keywords='pylint web2py plugin',
-    platforms=['any'],
-    packages=_packages,
-    install_requires=requires,
-    zip_safe=False,
+	name=pylint_web2py3.__name__,
+	url='https://github.com/vinyldarkscratch/pylint_web2py3',
+	version=pylint_web2py3.__version__,
+	description=pylint_web2py3.__doc__,
+	long_description=readme,
+	long_description_content_type="text/markdown",
+	author=pylint_web2py3.__author__[0],
+	author_email=pylint_web2py3.__email__[0],
+	maintainer=pylint_web2py3.__maintainer__,
+	maintainer_email=pylint_web2py3.__email__[0],
+	license='http://www.gnu.org/copyleft/gpl.html',
+	keywords='pylint web2py plugin',
+	platforms=['any'],
+	packages=_packages,
+	install_requires=requires,
+	zip_safe=False,
 )
